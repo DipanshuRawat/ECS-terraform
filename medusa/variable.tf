dcb0cc8836991bb80559314f8c45802c624c3b24
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-04-04 15:30:32 +0530

    Create variable.tf

---
+++ medusa/variable.tf
@@ -0,0 +1,52 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet1_cidr" {
+  description = "CIDR block for public subnet 1"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet2_cidr" {
+  description = "CIDR block for public subnet 2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "container_image" {
+  description = "Docker image for the Medusa container"
+  type        = string
+}
+
+variable "container_port" {
+  description = "Port exposed by the container"
+  type        = number
+  default     = 9000
+}
+
+variable "container_cpu" {
+  description = "CPU units for the container"
+  type        = number
+  default     = 256
+}
+
+variable "container_memory" {
+  description = "Memory for the container"
+  type        = number
+  default     = 512
+}
+
+variable "service_desired_count" {
+  description = "Number of tasks to run"
+  type        = number
+  default     = 1
+}
